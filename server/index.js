19c88d8303621df3ae3d01fbb60c0aac3e80575f
lyjchan0411 <lyjchan0411@gmail.com>
2021-04-19 18:54:50 -0400

    added .env file with the 8080 PORT - Jensen

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { PORT } = process.env;
 app.use(cors());
 app.use(express.json());
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log("Listening on port 8080");
 });
