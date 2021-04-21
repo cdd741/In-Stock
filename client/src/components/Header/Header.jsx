7fa42f2e99fcd9782f868aa16668b3247d1408c0
Joshua Sepulveda <sepulveda.62@gmail.com>
2021-04-20 23:51:04 -0400

    asset folders for team

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Header() {
+    return (
+        <header className='header' >
+            <img className='header__logo' src='/'/>
+            <ul>
+                <li></li>
+                
+            </ul>
+        </header>
+    )
+}
+
+export default Header
