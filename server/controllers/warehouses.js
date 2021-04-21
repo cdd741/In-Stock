4f3757344f208d47c89ec1eec553cc8a7c82c2d1
Joshua Sepulveda <sepulveda.62@gmail.com>
2021-04-21 15:39:20 -0400

    able to GET information of a single Warehouse by Id
    Signed-off-by: Joshua Sepulveda <sepulveda.62@gmail.com>

--- server/controllers/warehouses.js
+++ server/controllers/warehouses.js
@@ -9,6 +9,20 @@ const getWarehousesArr = (req,res)=>{
     res.status(200).json(warehouses)
 }
 
+//specific id for singlewarehouse
+const getWarehouseId = (req,res)=>{
+    const warehouse = warehouses.find((warehouse)=>{
+        return warehouse.id===req.params.id;
+    })
+    if(!warehouse){
+        return res
+        .status(404)
+        .json({success: false, msg:`warehouse ${req.params.id} is invalid`})
+    }
+    res.status(200).send(warehouse)
+}
+
 module.exports = {
     getWarehousesArr,
+    getWarehouseId,
 }
