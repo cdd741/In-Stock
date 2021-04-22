5e4dc068207866315d63d8dd85bd95f082e97169
lyjchan0411 <lyjchan0411@gmail.com>
2021-04-21 22:28:02 -0400

    added axios calls to get the inventories API and filter out for the specific location
    Signed-off-by: lyjchan0411 <lyjchan0411@gmail.com>

--- client/src/routes/WarehouseDetails.jsx
+++ client/src/routes/WarehouseDetails.jsx
@@ -1,11 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class WarehouseDetails extends Component {
-    render() {
-        return (
-            <div>
-                <p>THIS IS A PLACEHOLDER</p>
-            </div>
-        )
-    }
+  state = {
+    warehousesData: [],
+    inventoriesData: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:8080/inventories")
+      .then((res) => {
+        let inventories = res.data;
+        let warehouseId = this.props.match.params.id;
+        console.log(res.data);
+        let selectedLocation = inventories.filter(
+          (inventory) => inventory.warehouseID === warehouseId
+        );
+        console.log(selectedLocation);
+      })
+      .catch((err) => {
+        console.log("Cant receive inventories API ");
+      });
+  }
+
+  render() {
+    console.log(this.props.match.params.id);
+    return (
+      <div>
+        <p>THIS IS A PLACEHOLDER</p>
+      </div>
+    );
+  }
 }
