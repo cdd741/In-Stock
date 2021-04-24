e76bb87fd1f0ca372eda11b54e115f2e9b40750c
Andre Chen <b99chen@uwaterloo.ca>
2021-04-23 23:44:02 -0400

    formmating
    Signed-off-by: Andre Chen <b99chen@uwaterloo.ca>

--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -1,24 +1,20 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
 
-
 //controllers
-const{
-    getWarehousesArr,
-    getWarehouseId,
-    postWarehouse,
-    deleteWarehouse,
-    putWarehouse,
-} = require('../controllers/warehouses')
+const {
+  getWarehousesArr,
+  getWarehouseId,
+  postWarehouse,
+  deleteWarehouse,
+  putWarehouse,
+} = require("../controllers/warehouses");
 
 //routes for Warehouses
-router.get('/', getWarehousesArr)
-router.post('/', postWarehouse)
-router.get('/:id', getWarehouseId)
-router.delete('/:id', deleteWarehouse)
-router.put('/:id', putWarehouse)
+router.get("/", getWarehousesArr);
+router.post("/", postWarehouse);
+router.get("/:id", getWarehouseId);
+router.delete("/:id", deleteWarehouse);
+router.put("/:id", putWarehouse);
 
-
-
-
-module.exports = router
+module.exports = router;
