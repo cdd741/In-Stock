0cbc074812f885df960b97abb69bf5d44b6e817c
Joshua Sepulveda <sepulveda.62@gmail.com>
2021-04-24 23:25:36 -0400

    finishing edits and touch up/ functionality and stylying
    Signed-off-by: Joshua Sepulveda <sepulveda.62@gmail.com>

--- client/src/components/InventoryTable/InventoryTable.jsx
+++ client/src/components/InventoryTable/InventoryTable.jsx
@@ -3,14 +3,31 @@ import './InventoryTable.scss'
 import deleteIcon from '../../assets/icons/delete_outline-24px.svg'
 import editIcon from '../../assets/icons/edit2-24px.svg'
 import{Link} from 'react-router-dom';
+import sortIcon from '../../assets/icons/sort-24px.svg'
+import chevron from '../../assets/icons/chevron_right-24px.svg'
 
 function InventoryTable({inventory}) {
     const inventoryListTabletDesktop = inventory.map((item)=>{
         return(
             <tr className='table__row'>
                 <td className='table__data'>
-                    <Link to={`/inventories/${item.id}`}         className='table__edit'>{item.itemName}</Link></td>
-                <td className='table__data'>{item.status}</td>
+                    <Link to={`/inventories/${item.id}`} className='table__edit'>
+                        {item.itemName}
+                        <img className='table__Icon'src={chevron}/>
+                    </Link>
+                </td>
+                <td className='table__data '>
+                    {
+                    item.status==='In Stock'?
+                        (<div className='inStock'>
+                        {item.status}    
+                        </div>)
+                    :
+                        (<div className='outStock'>
+                        {item.status}    
+                        </div>)
+                    }
+                </td>
                 <td className='table__data'>{item.category}</td>
                 <td className='table__data'>{item.quantity}</td>
                 <td className='table__data'>{item.warehouseName}</td>
@@ -56,12 +73,12 @@ function InventoryTable({inventory}) {
                 {inventoryListMobile}
                 <table className='inventory__table'>
                     <tr className='table__row'>
-                        <th className='table__header'>INVENTORY ITEM</th>
-                        <th className='table__header'>STATUS</th>
-                        <th className='table__header'>CATEGORY</th>
-                        <th className='table__header'>QTY</th>
-                        <th className='table__header'>WAREHOUSE</th>
-                        <th className='table__header'>ACTION</th>
+                        <th className='table__header'>INVENTORY ITEM<img className='table__Icon' src={sortIcon} alt='sortIcon'/></th>
+                        <th className='table__header'>STATUS<img className='table__Icon' src={sortIcon}/></th>
+                        <th className='table__header'>CATEGORY<img className='table__Icon' src={sortIcon}/></th>
+                        <th className='table__header'>QTY<img className='table__Icon' src={sortIcon}/></th>
+                        <th className='table__header'>WAREHOUSE<img className='table__Icon' src={sortIcon}/></th>
+                        <th className='table__header'>ACTION<img className='table__Icon' src={sortIcon}/></th>
                     </tr>
                     {inventoryListTabletDesktop}
                 </table> 
