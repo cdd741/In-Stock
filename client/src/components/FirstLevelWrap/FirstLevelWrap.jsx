6a8916304aa27b0d531614df9cade8e0e86a3ee7
lyjchan0411 <lyjchan0411@gmail.com>
2021-04-25 19:07:47 -0400

    fixed button crashed bug on warehouse
    Signed-off-by: lyjchan0411 <lyjchan0411@gmail.com>

--- client/src/components/FirstLevelWrap/FirstLevelWrap.jsx
+++ client/src/components/FirstLevelWrap/FirstLevelWrap.jsx
@@ -131,6 +131,7 @@ export default class FirstLevelWrap extends Component {
                     type={"submit"}
                     className={"container__header-btn"}
                     children={"+ Add New Warehouse"}
+                    onClick={() => {}}
                   />
                 </Link>
               </form>
